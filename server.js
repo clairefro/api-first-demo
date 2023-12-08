85defbf39733335b1f2f8ecb45f1d88d9c80f478
Claire Froelich <claire.froelich@gmail.com>
2023-12-08 13:53:44 +0900

    add demo

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const beers = [[
+  {
+    "name": "Kirin",
+    "id": "abc",
+    "abv": 5
+  },
+  {
+    "name": "Sapporo",
+    "id": "efg",
+    "abv": 3
+  },
+  {
+    "name": "Asahi",
+    "id": "hgj",
+    "abv": 7
+  },
+  {
+    "name": "Budlight",
+    "id": "xyz",
+    "abv": 15
+  }
+]]
+
 // code your routes 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/beers', (req, res) => {
+  // TODO: fetch logic for getting beers from database goes here
+  res.send({ beers: beers })
 })
 
 // start server
